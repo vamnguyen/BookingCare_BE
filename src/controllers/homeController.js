89afd220db01611ae0c88ae7485ab78c136137ba
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-05-16 21:11:00 +0700

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -16,7 +16,11 @@ let getAboutPage = (req, res) => {
   return res.render('test/about.ejs');
 }
 
+let getCRUD = (req, res) => {
+  return res.render('crud.ejs')
+}
 module.exports = {
   getHomePage: getHomePage,
-  getAboutPage: getAboutPage
+  getAboutPage: getAboutPage,
+  getCRUD: getCRUD,
 }
