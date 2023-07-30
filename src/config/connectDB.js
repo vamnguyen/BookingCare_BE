080260f5106e179c39ab8a56633860400fa647e4
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-07-30 21:20:26 +0700

    error sequelize-cli not working :((

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 // Option 3: Passing parameters separately (other dialects)
 const sequelize = new Sequelize("bookingcare_demo", "root", null, {
-  host: "localhost",
+  host: "127.0.0.1",
   dialect: "mysql",
   logging: false,
 });
